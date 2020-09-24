702993394ab882e9202592b181a8b1373dafcfb5
fergorgs <fer.fayet@usp.br>
2020-09-24 19:20:56 -0300

    Player totalmente implementado
    - movimentos baseados em força
    - ghost jump e buffer jump
    
    - controle de respawn do player agora é um game object a parte

---
+++ Fuse Head/Assets/Scripts/RespawnControllerScript.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RespawnControllerScript : MonoBehaviour
+{
+	public Vector3[] respawnPoints;
+	public GameObject playerPrefab;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+	public void RespawnPlayer(int playerID, float secondsToRespawn)
+	{
+		StartCoroutine(RespawnPlayerTimer(playerID, secondsToRespawn));
+	}
+
+	private IEnumerator RespawnPlayerTimer(int playerID, float secondsToRespawn)          //playerID: diferenciar entre player 1, 2, 3 e 4
+	{
+		yield return new WaitForSeconds(secondsToRespawn);
+
+		Vector3 respPos = respawnPoints[Random.Range(0, respawnPoints.Length)];
+
+		Instantiate(playerPrefab, respPos, new Quaternion());
+	}
+}
