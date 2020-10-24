6354c1c0dc14b9bb934a3e4736f46c94822b28d4
Tiago M. Silva <tmsilva11@gmail.com>
2020-10-24 00:24:41 -0300

    Add Timer

---
+++ Fuse Head/Assets/Scripts/Timer.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class Timer : MonoBehaviour
+{
+    public UnityEvent OnCountdownEnd = null;
+    public float CountdownTime { get => _countdownTime; private set => _countdownTime = value; }
+    public float TimeElapsed { get; private set; } = 0;
+
+    [SerializeField] private float _countdownTime = 0;
+    [SerializeField] private float timeScale = 1;
+
+    private void Update()
+    {
+        TimeElapsed += Time.deltaTime * timeScale;
+
+        VerifyTime();
+    }
+
+    private void VerifyTime()
+    {
+        if(TimeElapsed >= _countdownTime)
+        {
+            OnCountdownEnd?.Invoke();
+            StopTimer();
+        }
+    }
+
+    public void StartTimer()
+    {
+        TimeElapsed = 0;
+        if (timeScale == 0f)
+            ContinueTimer();
+    }
+
+    public void StartTimer(float countdownTime)
+    {
+        _countdownTime = countdownTime;
+        StartTimer();
+    }
+
+    public void ContinueTimer() => timeScale = 1;
+    public void StopTimer() => timeScale = 0f;
+    public void UpdateTimeScale(float scale) => timeScale = scale;
+}
