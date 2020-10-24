57a6b4f218a85672449ca253a79475e253cae772
Tiago M. Silva <tmsilva11@gmail.com>
2020-10-24 17:45:45 -0300

    Add player animation transitions

---
+++ Fuse Head/Assets/Player/PlayerAnimationController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAnimationController : MonoBehaviour
+{
+    [SerializeField] private Animator _animator = null;
+    [SerializeField] private Rigidbody2D _rigidbody = null;
+    [SerializeField] private CharacterController2D _controller = null;
+
+    private void Awake()
+    {    
+        _rigidbody = GetComponent<Rigidbody2D>();
+        _animator = GetComponent<Animator>();
+        _controller = GetComponent<CharacterController2D>();
+
+        _controller.OnLandEvent.AddListener(TriggerLandEvent);
+    }
+
+    private void FixedUpdate()
+    {
+        _animator.SetFloat("Speed_X", _rigidbody.velocity.x);
+        _animator.SetFloat("Speed_Y", _rigidbody.velocity.y);
+    }
+
+    public void TriggerLandEvent()
+    {
+        _animator.SetTrigger("Landed");
+    }
+
+}
