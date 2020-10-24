7c2b4b7a0176c7392c0f0e042f33ea9384b6743f
Tiago M. Silva <tmsilva11@gmail.com>
2020-10-23 22:10:47 -0300

    Add Flag System
    Gets flags in scene and count captured flag to invoke Victory event

---
+++ Fuse Head/Assets/Scripts/FlagObject.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using UnityEngine;
+
+public class FlagObject : MonoBehaviour
+{
+    public Action OnFlagCaptured = null;
+
+	void OnCollisionEnter2D(Collision2D collision)
+	{
+        if (!collision.gameObject.CompareTag("Player"))
+            return;
+
+        OnFlagCaptured?.Invoke();
+		Destroy(gameObject);
+	}
+}
---
+++ Fuse Head/Assets/Scripts/FlagSystem.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System;
+using UnityEngine.Events;
+
+public class FlagSystem : MonoBehaviour
+{
+    [Header("Events")]
+    public UnityEvent OnGameVictory = new UnityEvent();
+
+    [Header("Flags")]
+    [Tooltip("Add the flags in scene you want to be considered for winning. Consider using contex menu Get Flags in Scene to automate")]
+    [SerializeField] private FlagObject[] _flags = null;
+    private int _remainingFlags = 0;
+
+    [ContextMenu("Get Flags in Scene")]
+    public void GetFlagsInScene()
+    {
+        _flags = FindObjectsOfType<FlagObject>();
+    }
+
+    public void Setup()
+    {
+        if(_flags == null || _flags.Length == 0)
+        {
+            GetFlagsInScene();
+            Debug.LogWarning($"Flag System {name} had no flags in array");
+        }
+
+        _remainingFlags = _flags.Length;
+        foreach(var flag in _flags)
+        {
+            flag.OnFlagCaptured += FlagCaptured;
+        }
+    }
+
+    private void FlagCaptured()
+    {
+        _remainingFlags--;
+        if (_remainingFlags <= 0)
+        {
+            OnGameVictory?.Invoke();
+            Debug.Log("Game Won");
+        }
+    }
+
+    private void Start()
+    {
+        Setup();
+    }
+}
