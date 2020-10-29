0b210a90b807e818e7f909d30d6d1010e71a96fb
firewall1011 <tmsilva11@gmail.com>
2020-10-28 23:59:51 -0300

    Update font and add auto company text

---
+++ Assets/UpdateCompanyText.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UpdateCompanyText : MonoBehaviour
+{
+    void Start()
+    {
+        GetComponent<TMPro.TMP_Text>().text = $"(c) {Application.companyName} {System.DateTime.Today.Year}";   
+    }
+}
