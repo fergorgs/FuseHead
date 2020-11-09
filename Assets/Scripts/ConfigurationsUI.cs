735ddf8397cd3ec0de583f23c016fbf8686370a0
Tiago M. Silva <tmsilva11@gmail.com>
2020-11-09 17:19:13 -0300

    Serialize Configurations
    Saving to ScriptableObject and Json

---
+++ Assets/Scripts/ConfigurationsUI.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class ConfigurationsUI : MonoBehaviour
+{
+    [System.Serializable]
+    private struct SoundSprites
+    {
+        public Image SfxImage;
+        public Sprite SfxOn;
+        public Sprite SfxOff;
+        public Image MusicImage;
+        public Sprite MusicOn;
+        public Sprite MusicOff;
+    }
+
+    [SerializeField]
+    private ConfigSO configs = null;
+
+    [SerializeField]
+    private SoundSprites soundSprites;
+
+
+    // Start is called before the first frame update
+    private void Awake()
+    {
+        LoadConfig();
+    }
+
+    public void SwitchSfx()
+    {
+        configs.SfxOn = !configs.SfxOn;
+        soundSprites.SfxImage.sprite = configs.SfxOn ? soundSprites.SfxOn : soundSprites.SfxOff;
+    }
+
+    public void SwitchMusic()
+    {
+        configs.MusicOn = !configs.MusicOn;
+        soundSprites.MusicImage.sprite = configs.MusicOn ? soundSprites.MusicOn : soundSprites.MusicOff;
+    }
+
+    private void LoadConfig()
+    {
+        configs.LoadFromFile();
+        soundSprites.SfxImage.sprite = configs.SfxOn ? soundSprites.SfxOn : soundSprites.SfxOff;
+        soundSprites.MusicImage.sprite = configs.MusicOn ? soundSprites.MusicOn : soundSprites.MusicOff;
+    }
+
+    private void OnDisable()
+    {
+        configs.SaveToFile();
+    }
+}
