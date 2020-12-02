1a3ba95e7e2614c2215b69f54b57ed9669b281f0
Tiago M. Silva <tmsilva11@gmail.com>
2020-12-01 21:59:00 -0300

    Add Defeat/Victory Text and Time Elapsed

--- Assets/Scripts/EndOfLevelUI.cs
+++ Assets/Scripts/EndOfLevelUI.cs
@@ -6,13 +6,23 @@ public class EndOfLevelUI : MonoBehaviour
 {
     [SerializeField] private SceneController sceneController = null;
     [SerializeField] private Button _retryBtn, _nextLevelBtn, _mainMenuBtn = null;
-    [SerializeField] private TextMeshProUGUI _headerText, _timeText = null;
+    [SerializeField] private TextMeshProUGUI _headerText;
 
     private void Awake()
     {
         _retryBtn?.onClick.AddListener(() => sceneController.ReloadActiveSceneWithTransition());
         _mainMenuBtn?.onClick.AddListener(() => sceneController.LoadSceneWithTransition(1));
         _nextLevelBtn?.onClick.AddListener(() => sceneController.LoadNextSceneWithTransition());
+    }
+
+    public void OnVictory()
+    {
+        _headerText.SetText("Victory");
+    }
+
+    public void OnGameOver()
+    {
+        _headerText.SetText("Defeat");
     }
 
 #if UNITY_EDITOR
@@ -24,7 +34,6 @@ public class EndOfLevelUI : MonoBehaviour
         _mainMenuBtn = transform.Find(nameof(_mainMenuBtn)).GetComponent<Button>();
 
         _headerText = transform.Find(nameof(_headerText)).GetComponent<TextMeshProUGUI>();
-        _timeText = transform.Find(nameof(_timeText)).GetComponent<TextMeshProUGUI>();
     }
 #endif
 }
