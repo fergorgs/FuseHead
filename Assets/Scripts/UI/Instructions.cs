c95595eed560f24a1ba650f8b10b34dbfb07d860
danbarretto <daniel_barretto98@hotmail.com>
2020-12-10 17:56:31 -0300

    Add instructions navigation

---
+++ Assets/Scripts/UI/Instructions.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Instructions : MonoBehaviour
+{
+    [SerializeField] private GameObject[] instructions;
+    [SerializeField] private GameObject prevBtn, nextBtn;
+    private int activeIndex = 0;
+
+    private void OnEnable()
+    {
+        foreach(var g in instructions){
+            g.SetActive(false);
+        }
+        activeIndex = 0;
+        instructions[activeIndex].SetActive(true);
+        prevBtn.SetActive(false);
+    }
+
+    public void NextInstruction()
+    {
+        if (activeIndex + 1 < instructions.Length){
+
+            instructions[activeIndex].SetActive(false);
+            activeIndex++;
+            instructions[activeIndex].SetActive(true);
+            if (activeIndex > 0 && activeIndex < instructions.Length -1)
+            {
+                prevBtn.SetActive(true);
+                nextBtn.SetActive(true);
+            }
+            else if (activeIndex == 0)
+            {
+                prevBtn.SetActive(false);
+                nextBtn.SetActive(true);
+            }
+            else if (activeIndex == instructions.Length - 1)
+            {
+                prevBtn.SetActive(true);
+                nextBtn.SetActive(false);
+            }
+        }
+        Debug.Log(activeIndex);
+    }
+
+    public void PreviousInstruction()
+    {
+        if (activeIndex - 1 >= 0){
+
+            instructions[activeIndex].SetActive(false);
+            activeIndex--;
+            instructions[activeIndex].SetActive(true);
+            if (activeIndex > 0 && activeIndex < instructions.Length)
+            {
+                prevBtn.SetActive(true);
+                nextBtn.SetActive(true);
+            }
+            else if (activeIndex == 0)
+            {
+                prevBtn.SetActive(false);
+                nextBtn.SetActive(true);
+            }
+            else if (activeIndex == instructions.Length - 1)
+            {
+                prevBtn.SetActive(true);
+                nextBtn.SetActive(false);
+            }
+        }
+    }
+
+}
