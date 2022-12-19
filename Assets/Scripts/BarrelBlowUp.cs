91b2c1340ee13d4372b915d8c20f30dbbc24e9ee
Fernando Gorgulho Fayet <fer.fayet@gmail.com>
2022-12-19 01:49:32 -0300

    Barril fully synched

--- Assets/Scripts/BarrelBlowUp.cs
+++ Assets/Scripts/BarrelBlowUp.cs
@@ -1,9 +1,10 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.Netcode;
 using UnityEngine;
 
-public class BarrelBlowUp : MonoBehaviour {
+public class BarrelBlowUp : NetworkBehaviour {
 
     public GameObject explosion;
 
@@ -14,7 +15,15 @@ public class BarrelBlowUp : MonoBehaviour {
 	}
 
     public void BlowUp() {
-        Instantiate(explosion, transform.position, transform.rotation);
+        //Instantiate(explosion, transform.position, transform.rotation);
+        SpawnExplosionServerRpc(transform.position, transform.rotation);
         Destroy(gameObject);
+    }
+
+    [ServerRpc]
+    private void SpawnExplosionServerRpc(Vector3 position, Quaternion rotation)
+    {
+        GameObject spawnedExplosion = Instantiate(explosion, transform.position, transform.rotation);
+        spawnedExplosion.GetComponent<NetworkObject>().Spawn(true);
     }
 }
