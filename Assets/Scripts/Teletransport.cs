561470775792b7995085b89d9ae00d25795fdb63
Fernando Gorgulho Fayet <fer.fayet@gmail.com>
2022-12-25 16:42:57 -0300

    Teletransport synched and working buttonlessly

--- Assets/Scripts/Teletransport.cs
+++ Assets/Scripts/Teletransport.cs
@@ -1,34 +1,34 @@
-﻿using UnityEngine;
+﻿using System.Collections.Generic;
+using UnityEngine;
 
-public class Teletransport : MonoBehaviour, IInteractable
+public class Teletransport : MonoBehaviour//, IInteractable
 {
     [SerializeField] private Teletransport target = null;
     [SerializeField] private AudioEvent teleportAudio = null;
     private AudioSource _audioSource = null;
 
-    [HideInInspector] public bool isTarget = false;
+    private HashSet<GameObject> arrivingPlayers = new HashSet<GameObject>();
 
     private void Awake()
     {
         _audioSource = GetComponent<AudioSource>();
     }
 
+    public void receivePlayer(GameObject player)
+    {
+        arrivingPlayers.Add(player);
+        player.transform.position = new Vector2(transform.position.x, transform.position.y + 1f);
+        teleportAudio?.Play(_audioSource);
+    }
+
     void OnTriggerEnter2D(Collider2D other)
     {
-        if (other.CompareTag("Player") && !isTarget)
-        {
-            target.isTarget = true;
-        }
+        if (other.CompareTag("Player") && !arrivingPlayers.Contains(other.gameObject))
+            target.receivePlayer(other.gameObject);
     }
 
     void OnTriggerExit2D(Collider2D other)
     {
-        if(isTarget) isTarget = false;
-    }
-
-    public void Interact(GameObject player)
-    {
-        player.transform.position = new Vector2(target.transform.position.x, target.transform.position.y + 1f);
-        teleportAudio?.Play(_audioSource);
+        arrivingPlayers.Remove(other.gameObject);
     }
 }
